b0bd6549389f278cfc97c8b456426417a3206077
Mykola Hutorov <hutorovmv@gmail.com>
2025-05-29 20:01:29 +0300

    fix(scripts): fix client cert generation folder

--- scripts/generate-client-cert.ps1
+++ scripts/generate-client-cert.ps1
@@ -1,4 +1,4 @@
 New-Item -ItemType Directory -Force -Path ./TodoList.Client/certs
 winget install FiloSottile.mkcert --accept-package-agreements --accept-source-agreements
 mkcert -install
-mkcert -key-file certs/key.key -cert-file certs/cert.crt localhost
+mkcert -key-file ./TodoList.Client/certs/key.key -cert-file ./TodoList.Client/certs/cert.crt localhost
